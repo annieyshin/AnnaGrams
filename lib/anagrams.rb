f69bcf95b1800313937a7be6398857510f31b8bc
Annie Shin <annieyshin@gmail.com>
2018-06-29 14:25:31 -0700

    add in vowel detection

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -4,23 +4,24 @@ class Word
     @word_two = word_two
   end
   def anagram_check()
-  puts "hope I make it to here"
-  puts @word_one
-  puts @word_two
-#1. check if the words are the same
+  vowel_list = ["a", "e", "i", "o", "u", "y"]
 # word_one_mod = @word_one.gsub!(/[^0-9A-Za-z]/, '')
 word_one_mod = @word_one.to_s.downcase()
-puts word_one_mod
+# puts word_one_mod
+if !word_one_mod.match("aeiouy")
+  puts "you have a vowel"
+else
+  puts "you have no vowels"
+end
+puts "here you are"
 # word_two_mod = @word_two.gsub!(/[^0-9A-Za-z]/, '')
 word_two_mod = @word_two.to_s.downcase()
-puts word_two_mod
-puts "here you are"
+# puts word_two_mod
+#2. check if the words contain vowels
 if word_one_mod.chars.sort == word_two_mod.chars.sort
   word_types = "anagrams"
 else word_types = "not equal"
 end
- #figure out how to alphabetize here
-#2. check if the words are actual dictionary words
 #3. check if anagrams or antigrams
     endOutput = ""
     endOutput = "#{word_one_mod} and #{word_two_mod} are #{word_types}"
