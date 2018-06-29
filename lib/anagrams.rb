87e33493b65ec466e908a092b8509ca8355717e5
Annie Shin <annieyshin@gmail.com>
2018-06-29 11:38:44 -0700

    Fail test 1 once more

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,20 +1,22 @@
 class Word
-  def initialize(word_list = {})
-    @word_one = word_list.fetch(:word_one, 'eat')
-    @word_two = word_list.fetch(:word_two, 'ate')
+  def initialize(word_one, word_two)
+    @word_one = word_one
+    @word_two = word_two
   end
 
   def anagram_check()
-  word_one = "eat"
-  word_two = "ate"
-  word_one_array = ""
-  word_two_array = ""
+  @word_one = "eat"
+  @word_two = "ate"
+  # word_one_array = ""
+  # word_two_array = ""
+  puts "hope I make it to here"
 
 
 #1. check if the words are the same
-word_one.gsub!(/[^0-9A-Za-z]/, '')
-word_two.gsub!(/[^0-9A-Za-z]/, '')
-if word_one.chars.sort == word_two.chars.sort
+@word_one.gsub!(/[^0-9A-Za-z]/, '')
+@word_two.gsub!(/[^0-9A-Za-z]/, '')
+puts "here you are"
+if @word_one.chars.sort == @word_two.chars.sort
   word_types = "anagram"
 end
 else word_types = "not equal"
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -4,8 +4,8 @@ require('anagrams')
 
 describe('#anagrams') do
   it('compares two words to see if eat and ate are anagrams') do
-    word_check = Word.new()
-    expect(comparison.anagram_check("eat", "ate")).to(eq("eat and ate are anagrams"))
+    word_check = Word.new("eat", "ate")
+    expect(comparison.anagram_check()).to(eq("eat and ate are anagrams"))
   end
   # it('compares two words to see if eat and ate are anagrams') do
   #   word_check = Word.new()
