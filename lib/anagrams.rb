d99685ee156d53061fe985c21dfcd959cc1edf19
Annie Shin <annieyshin@gmail.com>
2018-06-29 12:49:07 -0700

    PASS test 1, woohoo

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,34 +3,33 @@ class Word
     @word_one = word_one
     @word_two = word_two
   end
-
   def anagram_check()
   @word_one = "eat"
   @word_two = "ate"
-  word_one_array = []
-  word_two_array = []
+  # word_one_array = []
+  # word_two_array = []
   puts "hope I make it to here"
-
-
+  puts @word_one
+  puts @word_two
 #1. check if the words are the same
-word_one_mod = @word_one.gsub!(/[^0-9A-Za-z]/, '')
-word_two_mod = @word_two.gsub!(/[^0-9A-Za-z]/, '')
-word_one_array.push(word_one_mod)
-word_two_array.push(word_two_mod)
+# word_one_mod = @word_one.gsub!(/[^0-9A-Za-z]/, '')
+word_one_mod = @word_one.to_s
+puts word_one_mod
+# word_two_mod = @word_two.gsub!(/[^0-9A-Za-z]/, '')
+word_two_mod = @word_two.to_s
+puts word_two_mod
+# word_one_array.push(word_one_mod)
+# word_two_array.push(word_two_mod)
 puts "here you are"
 if word_one_mod.chars.sort == word_two_mod.chars.sort
-  word_types = "anagram"
-end
+  word_types = "anagrams"
 else word_types = "not equal"
+end
  #figure out how to alphabetize here
 #2. check if the words are actual dictionary words
 #3. check if anagrams or antigrams
-
-
     endOutput = ""
-    if word_one_array == word_two_array
-      endOutput = "The words #{word_one} and #{word_two} are #{word_types}"
-    end
+    endOutput = "#{word_one_mod} and #{word_two_mod} are #{word_types}"
     return endOutput
   end
 end
