6103a3904f7ec36ff850b9360650255efd23bbb9
Annie Shin <annieyshin@gmail.com>
2018-06-29 11:10:55 -0700

    Update the def initialize

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,18 +1,20 @@
 class Word
-  def initialize(text)
-    @words_to_check = text
+  def initialize(word_one, word_two)
+    @word_one = word_one
+    @word_two = word_two
   end
 
   def anagram_check()
   word_one = "eat"
   word_two = "ate"
-  word_types = ""
-  word_one_array = []
-  word_two_array = []
+  # word_types = ""
+  word_one_array = ""
+  word_two_array = ""
 
 
 #1. check if the words are the same
-  array #figure out how to alphabetize here
+word_one.chars.sort == word_two.chars.sort
+ #figure out how to alphabetize here
 #2. check if the words are actual dictionary words
 #3. check if anagrams or antigrams
 
