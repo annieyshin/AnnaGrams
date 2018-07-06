ba7ac47b69ef01047b5733be14c2698c70736a04
Guest User <Guest@Epicodus-3D.local>
2018-07-05 23:43:54 -0700

    add in code block

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,36 +1,42 @@
 class Word
-  def initialize(word_one, word_two)
-    @word_one = word_one
-    @word_two = word_two
-    #for when you want to input more than two words USE words = %w[cars scar for four creams scream racs]
-    #anagrams = words.group_by { |word| word.chars.sort }.values
+  def initialize(phrase)
+    @phrase = phrase
   end
+
   def anagram_check()
-  vowel_list = ["a", "e", "i", "o", "u", "y"]
-# word_one_mod = @word_one.gsub!(/[^0-9A-Za-z]/, '')
-word_one_mod = @word_one.to_s.downcase()
-# puts word_one_mod
-if word_one_mod.scan(/[aeiouy]/).count >= 1
-  puts "you have a vowel"
-else
-  puts "you need to put in actual words"
-end
+  endOutput = {}
+  word_split = @phrase.split(" ")
+  # word_split = @phrase.split(/\W+/)
+  word_split.each do |word|
+  # key = word.split('').join
+  key = word.split('').join('')
+  # string_attempt = word.join(', ')
+  # puts string_attempt
+  puts word.to_s.join(', ')
+  # word_sort = word_string.chars.sort.join("").to_s
+
+      # if endOutput.scan(/[aeiouy]/).count >= 1
+      #   puts "you have a vowel"
+      # else
+      #   puts "you need to put in actual words"
+      # end
+
 # word_two_mod = @word_two.gsub!(/[^0-9A-Za-z]/, '')
-word_two_mod = @word_two.to_s.downcase()
-if word_one_mod.chars.sort == word_two_mod.chars.sort
-  word_types = "anagrams"
-elsif word_one_mod.scan(/[word_two_mod]/).count == 0
-  word_types = "antigrams"
-elsif  word_one_mod.chars.sort != word_two_mod.chars.sort
-  word_types = "not anagrams nor antigrams"
-end
+
+
+#   word_types = "anagrams"
+# elsif word_one_mod.scan(/[word_two_mod]/).count == 0
+#   word_types = "antigrams"
+# elsif  word_one_mod.chars.sort != word_two_mod.chars.sort
+#   word_types = "not anagrams nor antigrams"
+# end
 #3. check if anagrams or antigrams
-    endOutput = ""
-    if word_one_mod.scan(/[aeiouy]/).count < 1
-    endOutput = "you need to put in actual words"
-    else
-    endOutput = "#{word_one_mod} and #{word_two_mod} are #{word_types}"
+    # endOutput = ""
+    # if word_split.scan(/[aeiouy]/).count < 1
+    # endOutput = "you need to put in actual words"
+    # else
+    # endOutput = "these words are #{word_types}"
     end
-    return endOutput
+
   end
 end
