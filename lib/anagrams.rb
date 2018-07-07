b8f4c557073efa9e8c6f0734df88e76846f89f02
Annie Shin <annieyshin@gmail.com>
2018-07-06 19:55:00 -0700

    try again to incorporate code block

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -6,31 +6,36 @@ class Word
     #anagrams = words.group_by { |word| word.chars.sort }.values
   end
   def anagram_check()
-  vowel_list = ["a", "e", "i", "o", "u", "y"]
-# word_one_mod = @word_one.gsub!(/[^0-9A-Za-z]/, '')
-word_one_mod = @word_one.to_s.downcase()
-# puts word_one_mod
-if word_one_mod.scan(/[aeiouy]/).count >= 1
-  puts "you have a vowel"
-else
-  puts "you need to put in actual words"
-end
-# word_two_mod = @word_two.gsub!(/[^0-9A-Za-z]/, '')
-word_two_mod = @word_two.to_s.downcase()
-if word_one_mod.chars.sort == word_two_mod.chars.sort
-  word_types = "anagrams"
-elsif word_one_mod.scan(/[word_two_mod]/).count == 0
-  word_types = "antigrams"
-elsif  word_one_mod.chars.sort != word_two_mod.chars.sort
-  word_types = "not anagrams nor antigrams"
-end
-#3. check if anagrams or antigrams
-    endOutput = ""
-    if word_one_mod.scan(/[aeiouy]/).count < 1
-    endOutput = "you need to put in actual words"
-    else
-    endOutput = "#{word_one_mod} and #{word_two_mod} are #{word_types}"
-    end
-    return endOutput
-  end
-end
+   endOutput = {}
+   word_split = @phrase.split(" ")
+   # word_split = @phrase.split(/\W+/)
+   word_split.each do |word|
+   # key = word.split('').join
+   key = word.split('').join('')
+   # string_attempt = word.join(', ')
+   # puts string_attempt
+   puts word.to_s.join(', ')
+   # word_sort = word_string.chars.sort.join("").to_s
+
+       # if endOutput.scan(/[aeiouy]/).count >= 1
+       #   puts "you have a vowel"
+       # else
+       #   puts "you need to put in actual words"
+       # end
+
+ # word_two_mod = @word_two.gsub!(/[^0-9A-Za-z]/, '')
+
+
+ #   word_types = "anagrams"
+ # elsif word_one_mod.scan(/[word_two_mod]/).count == 0
+ #   word_types = "antigrams"
+ # elsif  word_one_mod.chars.sort != word_two_mod.chars.sort
+ #   word_types = "not anagrams nor antigrams"
+ # end
+ #3. check if anagrams or antigrams
+     # endOutput = ""
+     # if word_split.scan(/[aeiouy]/).count < 1
+     # endOutput = "you need to put in actual words"
+     # else
+     # endOutput = "these words are #{word_types}"
+     end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -7,36 +7,36 @@ describe('#anagrams') do
     word_check = Word.new("eat", "ate")
     expect(word_check.anagram_check()).to(eq("eat and ate are anagrams"))
   end
-  it('compares two words to see if ruby and bury are anagrams') do
-    word_check = Word.new("ruby", "bury")
-    expect(word_check.anagram_check()).to(eq("ruby and bury are anagrams"))
-  end
-  it('compares two words to see if boohoo and rodent are anagrams') do
-    word_check = Word.new("boohoo", "rodent")
-    expect(word_check.anagram_check()).to(eq("boohoo and rodent are not anagrams nor antigrams"))
-  end
-  it('compares two words to see if bOHo and hOBo are anagrams') do
-    word_check = Word.new("bOHo", "hOBo")
-    expect(word_check.anagram_check()).to(eq("boho and hobo are anagrams"))
-  end
-  it('compares two words to see if bbb and ttt are anagrams') do
-    word_check = Word.new("bbb", "ttt")
-    expect(word_check.anagram_check()).to(eq("you need to put in actual words"))
-  end
-  it('compares two words to see if bbb and ttt are anagrams') do
-    word_check = Word.new("ccc", "ppp")
-    expect(word_check.anagram_check()).to(eq("you need to put in actual words"))
-  end
-  it('compares two words to see if naive and evian are anagrams') do
-    word_check = Word.new("naive", "evian")
-    expect(word_check.anagram_check()).to(eq("naive and evian are anagrams"))
-  end
-  it('compares two words to see if ttta and ttt are anagrams') do
-    word_check = Word.new("ttta", "ttt")
-    expect(word_check.anagram_check()).to(eq("ttta and ttt are not anagrams nor antigrams"))
-  end
-  it('compares two words to see if cat and by are anagrams') do
-    word_check = Word.new("cat", "by")
-    expect(word_check.anagram_check()).to(eq("cat and by are antigrams"))
-  end
+  # it('compares two words to see if ruby and bury are anagrams') do
+  #   word_check = Word.new("ruby", "bury")
+  #   expect(word_check.anagram_check()).to(eq("ruby and bury are anagrams"))
+  # end
+  # it('compares two words to see if boohoo and rodent are anagrams') do
+  #   word_check = Word.new("boohoo", "rodent")
+  #   expect(word_check.anagram_check()).to(eq("boohoo and rodent are not anagrams nor antigrams"))
+  # end
+  # it('compares two words to see if bOHo and hOBo are anagrams') do
+  #   word_check = Word.new("bOHo", "hOBo")
+  #   expect(word_check.anagram_check()).to(eq("boho and hobo are anagrams"))
+  # end
+  # it('compares two words to see if bbb and ttt are anagrams') do
+  #   word_check = Word.new("bbb", "ttt")
+  #   expect(word_check.anagram_check()).to(eq("you need to put in actual words"))
+  # end
+  # it('compares two words to see if bbb and ttt are anagrams') do
+  #   word_check = Word.new("ccc", "ppp")
+  #   expect(word_check.anagram_check()).to(eq("you need to put in actual words"))
+  # end
+  # it('compares two words to see if naive and evian are anagrams') do
+  #   word_check = Word.new("naive", "evian")
+  #   expect(word_check.anagram_check()).to(eq("naive and evian are anagrams"))
+  # end
+  # it('compares two words to see if ttta and ttt are anagrams') do
+  #   word_check = Word.new("ttta", "ttt")
+  #   expect(word_check.anagram_check()).to(eq("ttta and ttt are not anagrams nor antigrams"))
+  # end
+  # it('compares two words to see if cat and by are anagrams') do
+  #   word_check = Word.new("cat", "by")
+  #   expect(word_check.anagram_check()).to(eq("cat and by are antigrams"))
+  # end
 end
